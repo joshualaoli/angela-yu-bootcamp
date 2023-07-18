a1c2e91b8609f6b74f8d67d7be5b93bdb3429ebf
Joshua Laoli <joe.pixarts@gmail.com>
2023-07-18 10:04:31 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,19 @@
 const express = require ('express');
 const app = express ();
 
-app.listen (3000)
+app.get ('/', function (req, res) {
+    res.send ('Hello Joshua')
+})
+
+app.get ('/contact' ,  function (req, res) {
+    res.send ('Contact me at hello.joshualaoli@outlook.com')
+})
+
+app.get ('/about', function (req, res) {
+    res.send ('Hello Joshua Laoli here')
+    ')
+})
+
+app.listen (3000, function () {
+    console.log ('Hey, we use port 3000')
+})
